489d56ac8f4a7ccded96744595acb25170c94c00
Edi-Fatemi <61267499+Edi-Fatemi@users.noreply.github.com>
2020-06-14 01:12:14 +0430

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -4,14 +4,14 @@ const config = require("../config.json");
 
 module.exports.run = async (bot, message, args) => {
     
-    if (message.content== "$8ball"){
-    if (!args[2]) return message.reply("Lotfan Soal Kamel Beporsid.!");
+    
+    if (!eightBallArgs[2]) return message.reply("Lotfan Soal Kamel Beporsid.!");
     let replies = ["Yes.", "No.", "Nemidonam.", "Shayad.", "Badan Soal konid.", "Kiri.", "Kheyli.", "Chokh Ziba.", "Sikim Khiyari.", "Shomale Yadam Bere", "Kos'sher Nago.", "Dorogh Migi.", "Begoo Be Emam Majid.", "Be Qoran Karim'ina.", "Ye Dildo Talaei."];
 
     let result = Math.floor((Math.random() * result.length));
     let question = args.slice(1).join(" ");
 
-    let embed = new Discord.MessageEmbed()
+    let ballembed = new Discord.MessageEmbed()
     .setAuthor(bot.user.username)
     .setColor("#9307D8")
     .addField("Soal: ", question)
@@ -19,8 +19,8 @@ module.exports.run = async (bot, message, args) => {
     .setFooter(message.author.tag)
     .setTimestamp();
 
-    message.channel.send(embed);
-    }
+    message.channel.send(ballembed);
+    
 }
 
 
