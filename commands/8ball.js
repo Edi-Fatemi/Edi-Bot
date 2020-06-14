e5d43f572a28095792558f565f260bdfee535a34
Edi-Fatemi <61267499+Edi-Fatemi@users.noreply.github.com>
2020-06-14 17:00:37 +0430

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -3,23 +3,26 @@ const config = require("../config.json");
 
 
 module.exports.run = async (bot, message, args) => {
+    let questionArray = message.content.split(" ");
+    let questionArgs = questionArray.slice(bot.prefix.length+6);
 
-    
-    if (!args[2]) return message.reply("Lotfan Soal Kamel Beporsid.!");
-    let replies = ["Yes.", "No.", "Nemidonam.", "Shayad.", "Badan Soal konid.", "Kiri.", "Kheyli.", "Chokh Ziba.", "Sikim Khiyari.", "Shomale Yadam Bere", "Kos'sher Nago.", "Dorogh Migi.", "Begoo Be Emam Majid.", "Be Qoran Karim'ina.", "Ye Dildo Talaei."];
     let userArray = message.content.split(" ");
     let userArgs = userArray.slice(1);
     let member = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join(" ") || x.user.username === userArgs[0]) || message.member;
-    let result = Math.floor((Math.random() * replies.length));
-    let questionArray = message.content.split(" ");
-    let questionArgs = questionArray.slice(1);
+    
+    if (!questionArgs) return message.reply("Lotfan Soal Kamel Beporsid.!");
+
+    let replies= ["Yes.", "No.", "Nemidonam.", "Shayad.", "Badan Soal konid.", "Kiri.", "Kheyli.", "Chokh Ziba.", "Sikim Khiyari.", "Shomale Yadam Bere", "Kos'sher Nago.", "Dorogh Migi.", "Begoo Be Emam Majid.", "Be Qoran Karim'ina.", "Ye Dildo Talaei."];
+    
+    let result = replies[Math.floor(Math.random() * (replies.length))]
+    
 
     let ballembed = new Discord.MessageEmbed()
     .setAuthor(bot.user.username)
     .setColor("#9307D8")
     .setThumbnail(member.user.displayAvatarURL(), message.author.tag)
-    .addField("Soal: ", questionArray)
-    .addField("Javab: ", replies[result])
+    .addField("Soal: " + questionArgs)
+    .addField("Javab: " + replies[result])
     .setFooter(bot.user.username)
     .setTimestamp();
 
