b9f31306fce39edb0d4ffa837fc5d78c9434abe2
Edi-Fatemi <61267499+Edi-Fatemi@users.noreply.github.com>
2020-06-14 14:10:06 +0430

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -3,7 +3,8 @@ const config = require("../config.json");
 
 
 module.exports.run = async (bot, message, args) => {
-    
+    let questionArray = message.content.split(" ");
+    let questionArgs = args.slice(1).join(" ");
     
     if (!args[2]) return message.reply("Lotfan Soal Kamel Beporsid.!");
     let replies = ["Yes.", "No.", "Nemidonam.", "Shayad.", "Badan Soal konid.", "Kiri.", "Kheyli.", "Chokh Ziba.", "Sikim Khiyari.", "Shomale Yadam Bere", "Kos'sher Nago.", "Dorogh Migi.", "Begoo Be Emam Majid.", "Be Qoran Karim'ina.", "Ye Dildo Talaei."];
@@ -11,16 +12,15 @@ module.exports.run = async (bot, message, args) => {
     let userArgs = userArray.slice(1);
     let member = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join(" ") || x.user.username === userArgs[0]) || message.member;
     let result = Math.floor((Math.random() * replies.length));
-    let questionArray = message.content.split(" ");
-    let questionArgs = args.slice(2);
+
 
     let ballembed = new Discord.MessageEmbed()
     .setAuthor(bot.user.username)
     .setColor("#9307D8")
-    .setThumbnail(member.user.displayAvatarURL())
+    .setThumbnail(member.user.displayAvatarURL(), message.author.tag)
     .addField("Soal: ", questionArgs)
     .addField("Javab: ", replies[result])
-    .setFooter(message.author.tag)
+    .setFooter("https://cdn.discordapp.com/attachments/679511291794423830/719597839050604625/wallpaper.jpg")
     .setTimestamp();
 
     message.channel.send(ballembed);
