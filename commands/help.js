7b78a89e50daeaf3821551b8ae95e0d298e0c9d8
Edi-Fatemi <61267499+Edi-Fatemi@users.noreply.github.com>
2020-06-14 21:09:56 +0430

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -33,8 +33,8 @@ module.exports.run = async (bot, message, args) => {
             .setDescription('```help | hello | mute | unmute | addrole | 8ball \nremoverole | embed | kick | ban | meme | ping \nrestart | shutdown | uptime | gamers | cooldown \nsmasherbasher | mrhamed | eclipse | irpixel | clear \nmemberinfo```')
             .setThumbnail("https://cdn.discordapp.com/attachments/679511291794423830/719597839050604625/wallpaper.jpg")
             .addFields(
-                    { name: 'Prefix', value: '```$```', inline: true},
-                    {name: "Uptime", value:`**${days}d ${hours}h ${minutes}m ${seconds}s**`, inline:true})
+                    { name: 'Prefix', value: '```$```', inline: true },
+                    { name: "Uptime", value:`**${days}d ${hours}h ${minutes}m ${seconds}s**`, inline:true })
             .setColor('#9307D8')
             .setFooter(bot.user.username)
             .setTimestamp();
