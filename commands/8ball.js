61ae9d32cd8559848e3ba04578bd70699b8bac44
Edi-Fatemi <61267499+Edi-Fatemi@users.noreply.github.com>
2020-06-14 15:53:53 +0430

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -5,14 +5,14 @@ const config = require("../config.json");
 module.exports.run = async (bot, message, args) => {
 
     
-    if (!args[3]) return message.reply("Lotfan Soal Kamel Beporsid.!");
+    if (!args[2]) return message.reply("Lotfan Soal Kamel Beporsid.!");
     let replies = ["Yes.", "No.", "Nemidonam.", "Shayad.", "Badan Soal konid.", "Kiri.", "Kheyli.", "Chokh Ziba.", "Sikim Khiyari.", "Shomale Yadam Bere", "Kos'sher Nago.", "Dorogh Migi.", "Begoo Be Emam Majid.", "Be Qoran Karim'ina.", "Ye Dildo Talaei."];
     let userArray = message.content.split(" ");
     let userArgs = userArray.slice(1);
     let member = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join(" ") || x.user.username === userArgs[0]) || message.member;
     let result = Math.floor((Math.random() * replies.length));
     let questionArray = message.content.split(" ");
-    let questionArgs = args.slice(3);
+    let questionArgs = questionArray.slice(1);
 
     let ballembed = new Discord.MessageEmbed()
     .setAuthor(bot.user.username)
