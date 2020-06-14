d55f40d874731e79d52d8f5d5b3762021703ea0c
Edi-Fatemi <61267499+Edi-Fatemi@users.noreply.github.com>
2020-06-14 13:31:43 +0430

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -7,15 +7,15 @@ module.exports.run = async (bot, message, args) => {
     
     if (!args[2]) return message.reply("Lotfan Soal Kamel Beporsid.!");
     let replies = ["Yes.", "No.", "Nemidonam.", "Shayad.", "Badan Soal konid.", "Kiri.", "Kheyli.", "Chokh Ziba.", "Sikim Khiyari.", "Shomale Yadam Bere", "Kos'sher Nago.", "Dorogh Migi.", "Begoo Be Emam Majid.", "Be Qoran Karim'ina.", "Ye Dildo Talaei."];
-
+    let member = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join(" ") || x.user.username === userArgs[0]) || message.member;
     let result = Math.floor((Math.random() * replies.length));
     let questionArray = message.content.split(" ");
-    let questionArgs = args.slice(1);
+    let questionArgs = args.slice(2);
 
     let ballembed = new Discord.MessageEmbed()
     .setAuthor(bot.user.username)
     .setColor("#9307D8")
-    .setThumbnail("https://cdn.discordapp.com/attachments/679511291794423830/719597839050604625/wallpaper.jpg")
+    .setThumbnail(member.user.displayAvatarURL())
     .addField("Soal: ", questionArgs)
     .addField("Javab: ", replies[result])
     .setFooter(message.author.tag)
