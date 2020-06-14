47e746cc9ad38e507ad4489282befc018e4eae01
Edi-Fatemi <61267499+Edi-Fatemi@users.noreply.github.com>
2020-06-14 13:37:26 +0430

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -7,6 +7,8 @@ module.exports.run = async (bot, message, args) => {
     
     if (!args[2]) return message.reply("Lotfan Soal Kamel Beporsid.!");
     let replies = ["Yes.", "No.", "Nemidonam.", "Shayad.", "Badan Soal konid.", "Kiri.", "Kheyli.", "Chokh Ziba.", "Sikim Khiyari.", "Shomale Yadam Bere", "Kos'sher Nago.", "Dorogh Migi.", "Begoo Be Emam Majid.", "Be Qoran Karim'ina.", "Ye Dildo Talaei."];
+    let userArray = message.content.split(" ");
+    let userArgs = userArray.slice(1);
     let member = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join(" ") || x.user.username === userArgs[0]) || message.member;
     let result = Math.floor((Math.random() * replies.length));
     let questionArray = message.content.split(" ");
