27b78c41b879bd2f86a605b0c39fa781efd49880
Edi-Fatemi <61267499+Edi-Fatemi@users.noreply.github.com>
2020-06-14 23:39:58 +0430

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -7,6 +7,12 @@ typeof scores;
 
 module.exports.run = async (bot, message, args) => {
 
+    let days = Math.floor(bot.uptime / 86400000);
+    let hours = Math.floor(bot.uptime / 3600000) % 24;
+    let minutes = Math.floor(bot.uptime / 60000) % 60;
+    let seconds = Math.floor(bot.uptime / 1000) % 60;
+
+
     let userArray = message.content.split(" ");
     let userArgs = userArray.slice(1);
     let member = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join(" ") || x.user.username === userArgs[0]) || message.member;
@@ -15,8 +21,10 @@ module.exports.run = async (bot, message, args) => {
    const embed = new Discord.MessageEmbed()
         .setTitle("Gamers.")
         .setURL("https://discord.gg/SjBVuVv")  //("https://www.twitch.tv/e_d_i")
-        .setAuthor(member.user.tag, member.user.displayAvatarURL())
-        .addField({ name:"**Smasher Basher.**", value: "https://discord.gg/3ktaRPx", inline: true})
+        .setAuthor(member.user.tag, member.user.displayAvatarURL({dynamic: true}))
+        .addField({ name:"**Smasher Basher.**", value: "https://discord.gg/3ktaRPx", inline: true},
+                  {name: "uptime", value:`${days}d ${hours}h ${minutes}m ${seconds}s`, inline: true, dynamic: true}
+        )
         .setColor("#9307D8")
         .setThumbnail("https://cdn.discordapp.com/attachments/660199575872602126/682459904287834147/photo-1538481199705-c710c4e965fc.jpg")
         .setImage(bot.user.displayAvatarURL())
