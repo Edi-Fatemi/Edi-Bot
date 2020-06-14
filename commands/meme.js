e09ce9271ca45221c54698202ab62512ca645916
Edi-Fatemi <61267499+Edi-Fatemi@users.noreply.github.com>
2020-06-14 15:08:25 +0430

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -2,15 +2,22 @@ const Discord = require("discord.js");
 const randomPuppy = require("random-puppy");
 
 module.exports.run = async (bot, Message, args) => {
+    let userArray = message.content.split(" ");
+    let userArgs = userArray.slice(1);
+    let member = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join(" ") || x.user.username === userArgs[0]) || message.member;
+
+
     const subReddits = ["meme", "me_irl", "dankmeme"]
 
     const random = subReddits[Math.floor(Math.random() * subReddits.length)];
     const img = await randomPuppy(random);
 
     const embed = new Discord.MessageEmbed()
+    .setAuthor(member.user.tag, member.user.displayAvatarURL())
     .setImage(img)
     .setTitle(`From /r/${random}`)
     .setURL(`http://reddit.com/${random}`)
+    .setFooter(bot.user.username)
     .setTimestamp();
 
     Message.channel.send(embed);
