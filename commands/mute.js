92889a721d07cd37f7d854b966a80390b3f1e3fa
Edi-Fatemi <61267499+Edi-Fatemi@users.noreply.github.com>
2020-06-15 12:58:10 +0430

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -3,6 +3,10 @@ const config = require("../config.json");
 
 module.exports.run = async (bot, message, args) => {
     if (!message.member.hasPermission(['MANAGE_ROLES'])) return;
+
+    let userArray = message.content.split(" ");
+    let userArgs = userArray.slice(1);
+
     let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(" ") || x.user.username === args[0]);
     if(member.hasPermission(['MANAGE_ROLES']) && !message.member.hasPermission('MANAGE_ROLES')) return;
     
