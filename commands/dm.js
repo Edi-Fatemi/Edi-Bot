18aeeec067205e4691fd96924b9179c8b019ce29
Edi-Fatemi <61267499+Edi-Fatemi@users.noreply.github.com>
2020-06-26 02:25:30 +0430

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -7,8 +7,8 @@ module.exports.run = async (bot, message, args) => {
     if(!message.member.permissions.has("MANAGE_MESSAGES")) return message.channel.send("Shoma Permission Kafi Baraye Estefadeh Az In Command Ra Nadarid!");
     let user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
     if(!user) return message.channel.send(`Shoma Yek Member Ra Mention Nakardid, Ya Id Na'motabar Vared Kardid!`);
-    if(!args.slice(1).join(" ")) return message.channel.send("Message Khod Ra Moshakhas Konid!");
-    user.user.send(args.slice(1).join(" ")).catch(() => message.channel.send("Shoma Nemitavanid Be In User Message Mostaqim Befrestid!")).then(() => message.channel.send(`[${message.author.tag}] Yek Message Baraye [${user.user.tag}] Ferestad!`));
+    if(!args.slice(1)) return message.channel.send("Message Khod Ra Moshakhas Konid!");
+    user.user.send(args.slice(1)).catch(() => message.channel.send("Shoma Nemitavanid Be In User Message Mostaqim Befrestid!")).then(() => message.channel.send(`[${message.author.tag}] Yek Message Baraye [${user.user.tag}] Ferestad!`));
     
 }
 
