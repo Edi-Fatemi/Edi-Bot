0acd3fa50e6b09066876ea6f62aa1b8396028509
Edi-Fatemi <61267499+Edi-Fatemi@users.noreply.github.com>
2020-06-26 01:55:07 +0430

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -3,13 +3,12 @@ const config = require("../config.json");
 
 
 module.exports.run = async (bot, message, args) => {
-    let userArray = message.content.split(" ");
-    let userArgs = userArray.slice(1);
+    
     
     if(!message.member.permissions.has("MANAGE_MESSAGES")) return message.channel.send("Shoma Permission Kafi Baraye Estefadeh Az In Command Ra Nadarid!")
-    let user = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join(" ") || x.user.username === userArgs[0]) || message.member;
+    let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
     if(!user) return message.channel.send(`Shoma Yek Member Ra Mention Nakardid, Ya Id Na'motabar Vared Kardid!`);
-    if(!userArgs[1].join(" ")) return message.channel.send(" Message Khod Ra Moshakhas Konid!");
+    if(!args.slice(1).join(" ")) return message.channel.send(" Message Khod Ra Moshakhas Konid!");
     user.user.send(args.slice(1).join(" ")).catch(() => message.channel.send(" Shoma Nemitavanid Be In User Message Mostaqim Befrestid ")).then(() => message.channel.send(`[${message.author.tag}] Yek Message Baraye [${user.user.tag}] Ferestad!`));
 
 }
